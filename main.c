bba7a6cd36aa562334e36c10987511739230f920
woodsloki <tsands2@students.dacc.edu>
2026-09-16 16:58:25 +0000

    sync

--- main.c
+++ main.c
@@ -2,27 +2,53 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define MAX_COMMAND_LENGTH 512
-void handle_command(char* str);
+#define STATIC_ALOCATOR_MEMORY_SIZE (1024 * 1024)
+#define STATIC_ALOCATOR_MAX_MALLOC 1024
+
+struct StaticAlocatorConnection {
+  uint64_t pos;
+  uint64_t size;
+};
+
+
+typedef struct StaticAlocator {
+  uint8_t memory[STATIC_ALOCATOR_MEMORY_SIZE];
+
+  uint64_t connection_count;
+  // this is sorted by pos
+  struct StaticAlocatorConnection connections[STATIC_ALOCATOR_MAX_MALLOC];
+} StaticAlocator;
+
+static StaticAlocator alocator;
+
+void initStaticAlocator(StaticAlocator* stal) {
+  stal->connection_count = 0;
+  for (uint64_t i = 0; i < STATIC_ALOCATOR_MAX_MALLOC; i++) {
+    stal->connections[i].pos  = (uint64_t)(0); // = NULL
+    stal->connections[i].size = (uint64_t)(0); // = NULL
+  }
+  
+  for (uint64_t b = 0; b < STATIC_ALOCATOR_MEMORY_SIZE; b++) {
+    stal->memory[b] = (uint8_t)(0);
+  }
+}
+
+void* my_malloc(size_t size) {
+  uint64_t slot = alocator.connection_count;
+  alocator.connection_count++;
+  uint64_t pos = 0;
+
+  // resorting the connections
+  uint64_t currect_slot = slot;
+  
+  
+}
 
 int main(int argc, char *arga[]) {
   printf("\033c"); fflush(stdout);
-  char command[MAX_COMMAND_LENGTH];
-  while (1) {
-    printf(">");
-    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {}
 
-    handle_command(command);
-  }
+
   
   return 0;
 }
 
-char** parce_command(char* command) {
-  
-}
-
-void handle_command(char* str) {
-  printf("%s\n", str);
-  return;
-}
